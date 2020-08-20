35cd8ff49d90ae01c6f1518eb424323518881508
Andrea <andrea@dellacorte.me>
2020-08-20 17:29:54 +0100

    Create logo collection

---
+++ imports/entities/logos.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Logos = new Mongo.Collection('logos');
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
+import '../imports/entities/tasks.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
